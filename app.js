5f7640761e5d28f9ca04042139a6ec1ed064524a
lsarah-kl <109840441+lsarah-kl@users.noreply.github.com>
2024-03-08 01:32:45 +0100

    app.js frontend

---
+++ app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Frontend components and logic */}
+      <h1>Music Recommender MVP</h1>
+    </div>
+  );
+}
+
+export default App;
